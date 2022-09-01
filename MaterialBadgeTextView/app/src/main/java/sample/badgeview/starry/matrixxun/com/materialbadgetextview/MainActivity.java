32c75814e930464e1db3a3a0141cbd7f15f2213d
CrazyXacker <1788475+CrazyXacker@users.noreply.github.com>
2022-09-02 00:00:49 +0300

    Migrate into AndroidX

--- MaterialBadgeTextView/app/src/main/java/sample/badgeview/starry/matrixxun/com/materialbadgetextview/MainActivity.java
+++ MaterialBadgeTextView/app/src/main/java/sample/badgeview/starry/matrixxun/com/materialbadgetextview/MainActivity.java
@@ -2,11 +2,11 @@ package sample.badgeview.starry.matrixxun.com.materialbadgetextview;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
